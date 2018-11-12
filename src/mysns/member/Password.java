8686d00a2f1d19dacd5b4def4ac7551122a4bae8
hjw <eeject@gmail.com>
2018-11-12 12:51:28 +0900

    first commit

---
+++ src/mysns/member/Password.java
@@ -0,0 +1,14 @@
+package mysns.member;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class Password {
+
+/*	String hashed = BCrypt.hashpw(password, BCrypt.gensalt(11));
+
+
+	if (BCrypt.checkpw(candidate, hashed))  
+	    System.out.println("It matches");
+	else  
+	    System.out.println("It does not match");*/
+}
